34dc564225926e9aca664b6a1ecf3016a838cdee
GalenReed <galenreed@vt.edu>
2024-12-23 12:43:11 -0500

    pets!

--- lib/owner_pet.py
+++ lib/owner_pet.py
@@ -1,5 +1,44 @@
-class Pet:
-    pass
-
 class Owner:
-    pass
+    def __init__(self, name):
+        self.name = name
+        self._pets = []
+
+    def pets(self):
+        return self._pets
+
+    def add_pet(self, pet):
+        if isinstance(pet, Pet):
+            self._pets.append(pet)
+            pet.owner = self
+        else:
+            raise Exception("Invalid pet type. Must be an instance of Pet")
+
+    def get_sorted_pets(self):
+        return sorted(self._pets, key=lambda pet: pet.name)
+
+class Pet:
+
+    PET_TYPES = ["dog", "cat", "rodent", "bird", "reptile", "exotic"]
+    all = []
+
+    def __init__(self, name, pet_type, owner=None):
+        self.name = name
+
+        if isinstance(owner, Owner):
+            self.owner = owner
+            owner.add_pet(self)
+        elif owner is None:
+            self.owner = None
+        else:
+            raise Exception("Owner must be an instance of Owner or None.") 
+        
+        if pet_type in self.PET_TYPES:
+            self.pet_type = pet_type
+        else:
+            raise Exception({"Invalid pet type"})
+        
+        self.all.append(self)
+
+
+
+    
